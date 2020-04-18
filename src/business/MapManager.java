8091c97c7e8d468e172b025ee7bbd62883507869
clashoflegends <clashoflegends.thegame@gmail.com>
2020-04-17 21:08:41 -0600

    All elements are on the map now.

--- src/business/MapManager.java
+++ src/business/MapManager.java
@@ -5,12 +5,21 @@
  */
 package business;
 
+import business.facade.ArtefatoFacade;
 import business.facade.CidadeFacade;
+import business.facade.ExercitoFacade;
+import business.facade.JogadorFacade;
 import business.facade.LocalFacade;
+import business.facade.PersonagemFacade;
+import control.WorldFacadeCounselor;
 import gui.drawings.DrawingFactory;
 import helpers.Hexagon;
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import javafx.geometry.Point2D;
 import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
@@ -19,8 +28,14 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
+import model.Artefato;
 import model.Cidade;
+import model.Exercito;
+import model.Habilidade;
+import model.Jogador;
 import model.Local;
+import model.Nacao;
+import model.Personagem;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import persistence.local.ImageFactory;
@@ -34,18 +49,19 @@ import persistenceCommons.SettingsManager;
 public class MapManager {
 
     private static final Log log = LogFactory.getLog(MapManager.class);
-    private final ImageFactory imageFactory = new ImageFactory();
-    private final DrawingFactory drawingFactory = new DrawingFactory();
-    private final LocalFacade localFacade = new LocalFacade();
-    private final CidadeFacade cityFacade = new CidadeFacade();
-    private Point farPoint;
-    private int xHexes;
-    private int yHexes;
-    private final int hexSize;
-    private final int coordinateFontSize;
-    private final double zoomFactor;
     private static final int SCROLLBAR_SIZE = 15;
-    private final boolean renderGrid;
+    private final ImageFactory imageFactory;
+    private final DrawingFactory drawingFactory;
+    private final LocalFacade localFacade;
+    private final CidadeFacade cityFacade;
+    private final ArtefatoFacade itemFacade;
+    private final PersonagemFacade persFacade;
+    private final JogadorFacade playerFacade;
+    private final ExercitoFacade armyFacade;
+    private final int hexSize;
+    private int coordinateFontSize;
+    private double zoomFactor;
+    private boolean renderGrid;
     private boolean renderRoads;
     private boolean renderRivers;
     private boolean renderCreek;
@@ -57,34 +73,52 @@ public class MapManager {
     private boolean renderLandmark;
     private boolean renderCities;
     private boolean renderForts;
+    private boolean renderArmy;
+    private boolean renderFeatures;
+    private boolean armyIconDrawType;
+    private Point farPoint;
+    private int xHexes;
+    private int yHexes;
+    private Jogador observer;
+    final int[][] armySpacing4 = {{7, 36}, {17, 42}, {34, 42}, {46, 36}};
+    final int[][] armySpacing6 = {{6, 36}, {12, 39}, {18, 42}, {36, 42}, {40, 39}, {46, 36}};
+    final int[][] armySpacing8 = {{6, 36}, {12, 39}, {18, 42}, {24, 45}, {30, 45}, {36, 42}, {40, 39}, {46, 36}};
 
     public MapManager() {
+        this.itemFacade = new ArtefatoFacade();
+        this.cityFacade = new CidadeFacade();
+        this.localFacade = new LocalFacade();
+        this.persFacade = new PersonagemFacade();
+        this.playerFacade = new JogadorFacade();
+        this.armyFacade = new ExercitoFacade();
+        this.drawingFactory = new DrawingFactory();
+        this.imageFactory = new ImageFactory();
         this.hexSize = 60;
         //TODO: refactor map coordinates label to be a Text over canvas, out of GraphicsContext so that we can use CSS
-        //FIXME: initializing on constructor will not work once we move to animation
-        renderGrid = SettingsManager.getInstance().isConfig("MapGridRender", "1", "0");
-        zoomFactor = (double) SettingsManager.getInstance().getConfigAsInt("MapZoomPercent", "100") / 100;
-        coordinateFontSize = (int) Math.round(SettingsManager.getInstance().getConfigAsInt("MapCoordinatesSize", "8") / zoomFactor);
     }
 
     public Canvas getCanvas() {
         log.info("Start map");
         setRenderingFlags();
+        //set observer
+        observer = WorldFacadeCounselor.getInstance().getJogadorAtivo();
+        //load hexes
         ListFactory listFactory = new ListFactory();
         Collection<Local> listaLocal = listFactory.listLocais().values();
         if (farPoint == null) {
+            //calculate max size for map
             getMapMaxSize(listaLocal);
         }
 
+        //start rendering
         Canvas canvas = new Canvas(xHexes * hexSize * zoomFactor, (yHexes * hexSize * 3 / 4 + SCROLLBAR_SIZE) * zoomFactor);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.scale(zoomFactor, zoomFactor);
         gc.setFill(Color.GAINSBORO);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
         doRenderTerrain(gc, listaLocal);
-        if (renderGrid) {
-            doRenderHexagonGrid(gc, listaLocal);
-        }
+        doRenderHexagonGrid(gc, listaLocal);
+
         log.info("finish map");
         return canvas;
     }
@@ -96,9 +130,12 @@ public class MapManager {
             //draw terrain
             gc.drawImage(imageFactory.getTerrainImage(local), point.getX(), point.getY());
             //roads, rivers, bridges, span, creek, landing, army tracks
-            doRenderDecorations(local, gc, point);
+            doRenderDecoration(local, gc, point);
             doRenderCity(local, gc, point);
-
+            doRenderLandmark(local, gc, point);
+            doRenderFeatures(local, gc, point);
+            doRenderArmy(local, gc, point);
+            doRenderFogofWar(local, gc, point);
         }
     }
 
@@ -143,7 +180,132 @@ public class MapManager {
         }
     }
 
-    private void doRenderDecorations(Local local, GraphicsContext gc, Point2D point) {
+    private void doRenderArmy(Local local, GraphicsContext gc, Point2D point) {
+        if (!renderArmy || localFacade.getExercitos(local).isEmpty()) {
+            return;
+        }
+        //decide which type of list to use
+        Collection<Nacao> armyList;
+        if (localFacade.getExercitos(local).size() < 9 && armyIconDrawType) {
+            armyList = listNationArmies();
+        } else {
+            armyList = listNationArmiesDeDup();
+        }
+        //list nations 
+        for (Exercito exercito : localFacade.getExercitos(local).values()) {
+            armyList.add(armyFacade.getNacao(exercito));
+        }
+        //Decide spacing
+        int[][] armySpacing = doArmySpacing(armyList.size());
+        //render icons
+        int nn = 0;
+        for (Nacao nation : armyList) {
+            //FIXME: imprimir exercito de nacao desconhecida???
+            final Image img = imageFactory.getArmyShield(nation, WorldFacadeCounselor.getInstance().getCenario());
+            gc.drawImage(img, point.getX() + armySpacing[nn][0], point.getY() + armySpacing[nn][1]);
+            nn++;
+        }
+    }
+
+    private Collection<Nacao> listNationArmies() {
+        final List<Nacao> armyList = new ArrayList<>();
+        return armyList;
+    }
+
+    private Collection<Nacao> listNationArmiesDeDup() {
+        final Set<Nacao> armyList = new HashSet<>();
+        //monta a lista das nacoes com exercitos presentes no local
+        return armyList;
+    }
+
+    private int[][] doArmySpacing(int size) {
+        int[][] armySpacing;
+        if (size < 5) {
+            armySpacing = armySpacing4;
+        } else if (size < 7) {
+            armySpacing = armySpacing6;
+        } else {
+            armySpacing = armySpacing8;
+        }
+        return armySpacing;
+    }
+
+    private void doRenderLandmark(Local local, GraphicsContext gc, Point2D point) {
+        if (!renderLandmark || !local.isVisible() || !localFacade.isTerrainLandmark(local)) {
+            return;
+        }
+        //terrain features
+        for (Habilidade landmark : localFacade.getTerrainLandmark(local)) {
+            //imprime gold mine
+            final Image img = imageFactory.getLandmark(landmark.getCodigo());
+            gc.drawImage(img, point.getX() + (hexSize - img.getWidth()) / 2, point.getY() + (hexSize - img.getHeight()) / 2);
+        }
+    }
+
+    private void doRenderFeatures(Local local, GraphicsContext gc, Point2D point) {
+        //TODO: Make animation!
+        if (!renderFeatures) {
+            return;
+        }
+        //render ships
+        if (localFacade.isBarcos(local)) {
+            gc.drawImage(ImageFactory.getShipsImage(), point.getX() + 34, point.getY() + 30);
+        }
+        //renders combat icon
+        if (local.isVisible() && localFacade.isCombatTookPlace(local)) {
+            //what type of combat?
+            if (localFacade.isCombatTookPlaceBigNavy(local)) {
+                gc.drawImage(ImageFactory.getCombatBigNavyImage(), point.getX() + 46, point.getY() + 30);
+            } else if (localFacade.isCombatTookPlaceBigArmy(local)) {
+                gc.drawImage(ImageFactory.getCombatBigArmyImage(), point.getX() + 46, point.getY() + 30);
+            } else {
+                //none of the above
+                gc.drawImage(ImageFactory.getCombatImage(), point.getX() + 46, point.getY() + 30);
+            }
+        }
+        //render overrun
+        if (local.isVisible() && localFacade.isOverrunTookPlace(local)) {
+            gc.drawImage(ImageFactory.getExplosionImage(), point.getX() + 40, point.getY() + 36);
+        }
+        //render items
+        for (Artefato item : localFacade.getArtefatos(local).values()) {
+            if (itemFacade.isPosse(item)) {
+                gc.drawImage(ImageFactory.getItemKnownImage(), point.getX() + 11, point.getY() + 22);
+            } else {
+                gc.drawImage(ImageFactory.getItemLostImage(), point.getX() + 46, point.getY() + 22);
+            }
+        }
+        //render chars
+        for (Personagem pers : localFacade.getPersonagens(local).values()) {
+            if (persFacade.isNpc(pers)) {
+                gc.drawImage(ImageFactory.getCharNpcImage(), point.getX() + 12, point.getY() + 19);
+            } else if (playerFacade.isMine(pers, observer)) {
+                gc.drawImage(ImageFactory.getCharImage(), point.getX() + 04, point.getY() + 22);
+            } else if (playerFacade.isAlly(pers, observer)) {
+                gc.drawImage(ImageFactory.getCharAllyImage(), point.getX() + 7, point.getY() + 26);
+            } else {
+                gc.drawImage(ImageFactory.getCharOtherImage(), point.getX() + 12, point.getY() + 23);
+            }
+        }
+    }
+
+    private void doRenderFogofWar(Local local, GraphicsContext gc, Point2D point) {
+        if (local.isVisible() || SettingsManager.getInstance().isWorldBuilder()) {
+            return;
+        }
+        if (!SettingsManager.getInstance().isConfig("FogOfWarType", "1", "1")) {
+            return;
+        }
+        //render fog of war
+        final Image img = ImageFactory.getFogofwarImage();
+        gc.drawImage(img, point.getX() + (hexSize - img.getWidth()) / 2, point.getY() + (hexSize - img.getHeight()) / 2);
+        //FIXME: test fog of war, adjust alpha
+//        big.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .6f));
+//        big.drawImage(this.desenhoDetalhes[dtFogofwar], x, y, form);
+//        big.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+    }
+
+    private void doRenderDecoration(Local local, GraphicsContext gc, Point2D point) {
         for (int direction = 1; direction < 7 && directionBased; direction++) {
             //draw roads
             if (renderRoads && localFacade.isEstrada(local, direction)) {
@@ -177,24 +339,11 @@ public class MapManager {
         }
     }
 
-    private void setRenderingFlags() {
-        //TODO: load from settings
-        //direction based
-        renderRoads = true;
-        renderRivers = true;
-        renderCreek = true;
-        renderBridge = true;
-        renderSpan = true;
-        renderTracks = true;
-        renderLanding = true;
-        directionBased = renderRoads && renderRivers && renderCreek && renderBridge && renderSpan && renderTracks && renderLanding;
-        //other decorations
-        renderLandmark = true;
-        renderCities = true;
-        renderForts = true;
-    }
-
     private void doRenderHexagonGrid(GraphicsContext gc, Collection<Local> listLocal) {
+        if (!renderGrid) {
+            return;
+        }
+
         //prepping styles
         gc.setTextAlign(TextAlignment.CENTER);
         gc.setTextBaseline(VPos.CENTER);
@@ -260,5 +409,29 @@ public class MapManager {
         xHexes = ret[1];
         yHexes = ret[0];
         return this.farPoint;
+    }
+
+    private void setRenderingFlags() {
+        //TODO: load from settings
+        //direction based
+        renderRoads = true;
+        renderRivers = true;
+        renderCreek = true;
+        renderBridge = true;
+        renderSpan = true;
+        renderTracks = true;
+        renderLanding = true;
+        directionBased = renderRoads && renderRivers && renderCreek && renderBridge && renderSpan && renderTracks && renderLanding;
+        //other decorations
+        renderLandmark = true;
+        renderCities = true;
+        renderForts = true;
+        renderArmy = true;
+        renderFeatures = true;
+        //FIXME: initializing here will not work once we move to animation
+        renderGrid = SettingsManager.getInstance().isConfig("MapGridRender", "1", "0");
+        zoomFactor = (double) SettingsManager.getInstance().getConfigAsInt("MapZoomPercent", "100") / 100;
+        coordinateFontSize = (int) Math.round(SettingsManager.getInstance().getConfigAsInt("MapCoordinatesSize", "8") / zoomFactor);
+        armyIconDrawType = SettingsManager.getInstance().isConfig("DrawAllArmyIcons", "1", "1");
     }
 }
--- src/persistence/local/ImageFactory.java
+++ src/persistence/local/ImageFactory.java
@@ -8,8 +8,13 @@ package persistence.local;
 import business.converter.ConverterFactory;
 import business.facade.LocalFacade;
 import business.MapManager;
+import business.facade.NacaoFacade;
+import control.WorldFacadeCounselor;
+import java.util.SortedMap;
 import javafx.scene.image.Image;
+import model.Cenario;
 import model.Local;
+import model.Nacao;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import persistenceCommons.SettingsManager;
@@ -22,6 +27,7 @@ public class ImageFactory {
 
     private static final Log log = LogFactory.getLog(MapManager.class);
     private static final LocalFacade localFacade = new LocalFacade();
+    private static final NacaoFacade nationFacade = new NacaoFacade();
     private static final String[] bridgeNames = {"empty", "ponte_no", "ponte_ne", "ponte_l", "ponte_se", "ponte_so", "ponte_o"};
     private static final String[] creekNames = {"empty", "riacho_no", "riacho_ne", "riacho_l", "riacho_se", "riacho_so", "riacho_o"};
     private static final String[] landingNames = {"empty", "water_NW", "water_NE", "water_E", "water_SE", "water_SW", "water_W"};
@@ -33,6 +39,38 @@ public class ImageFactory {
     private static final String[] cityNames = {"vazio", "acampamento", "aldeia", "vila", "burgo", "cidade"};
     private static final String[] hiddenCityNames = {"vazio", "acampamento_hidden", "aldeia_hidden", "vila_hidden", "burgo_hidden", "cidade_hidden"};
     private static final String[] dockNames = {"vazio", "docas", "porto"};
+    private final SortedMap<String, String> landmarkNames;
+    private static final String[] shieldsLom = {"neutral.png", "lom_corelay.png", "lom_ashimar.png", "lom_gloom.png", "lom_dregrim.png", "lom_despair.png",
+        "lom_korkithdodrak.png", "lom_valethor.png", "lom_kor.png", "neutral2.png", "neutral3.png", "lom_wolf.png", "lom_skulkrin.png", "lom_icetrolls.png", "lom_dragon.png"
+    };
+    private static final String[] shieldsAll = {"neutral.png", "KingsCourt.gif", "Jofrey.png", "Arryn.png", "Baratheon.gif", "Greyjoy.gif", "Lannister.gif",
+        "Martell.png", "Stark.gif", "Targaryen.gif", "Tully.png", "Tyrell.gif", "NightsWatch.png", "Bolton.png", "Yronwood.png", "Stannis.gif", "Frey.png",
+        "Hightower.gif", "Volantis.png", "Pentos.png", "Braavos.png", "FreeCities.png", "Wildlings.png", "neutral2.png", "neutral3.png",
+        "Esparta.gif", "Atenas.gif", "Macedonia.gif", "Persia.gif", "Tracia.gif", "Milletus.gif", "Illyria.gif", "Epirus.gif", "Twainek.gif", "Frusodian.gif",
+        "lom_corelay.png", "lom_ashimar.png", "lom_gloom.png", "lom_dregrim.png", "lom_despair.png", "lom_korkithdodrak.png", "lom_valethor.png", "lom_kor.png"
+    };
+    private static final String[] shieldsElse = {"neutral.png", "KingsCourt.gif", "Arryn.png", "Baratheon.gif", "Greyjoy.gif", "Lannister.gif", "Martell.png",
+        "Stark.gif", "Targaryen.gif", "Tully.png", "Tyrell.gif", "NightsWatch.png", "FreeCities.png", "Wildlings.png", "neutral2.png", "neutral3.png", "Bolton.png",
+        "Yronwood.png", "Stannis.gif", "Frey.png", "Hightower.gif", "Volantis.png", "Pentos.png", "Jofrey.png", "army1.png", "army2.png", "army3.png", "Esparta.gif",
+        "Atenas.gif", "Macedonia.gif", "Persia.gif", "Tracia.gif", "Milletus.gif", "Illyria.gif", "Epirus.gif", "Twainek.gif", "Frusodian.gif", "shield.jpg"
+    };
+    private static final String[] shieldsWdo = {"neutral.png", "wdor_carndun.gif", "wdor_gram.gif", "wdor_gundabad.png", "wdor_highpass.gif", "wdor_moria.png",
+        "wdor_methedras.gif", "wdor_morannon.png", "wdor_dolguldur.png", "wdor_wargriders.gif", "wdor_grey.png", "wdob_rangers.png", "wdob_rivendell.png",
+        "wdob_beornings.png", "wdob_silvan.png", "wdob_menlake.png", "wdob_longbeard.gif", "wdob_broadbeams.png", "wdob_stonefist.png", "wdob_ironhelm.png",
+        "wdob_blackhammer.png", "wdob_dorwinion.gif", "wdob_halfling.png", "wdob_lindon.png", "wdob_lorien.gif", "wdob_lossoth.png", "wdob_northmen.png",
+        "wdob_rohan.gif", "wdob_woodmen.png", "wdob_woses.png", "wdor_baltoch.gif", "wdor_druedain.gif", "wdor_forochel.gif", "wdor_mistygoblin.gif", "wdor_northorcs.png",
+        "wdor_raiders.gif", "wdor_variags.gif", "wdor_wainriders.gif", "wdon_neutral1.gif"
+    };
+    private static final String[] shieldsGot = {"neutral.png", "KingsCourt.gif", "Arryn.png", "Baratheon.gif", "Greyjoy.gif", "Lannister.gif", "Martell.png",
+        "Stark.gif", "Targaryen.gif", "Tully.png", "Tyrell.gif", "NightsWatch.png", "FreeCities.png", "Wildlings.png", "neutral2.png", "neutral3.png",
+        "Bolton.png", "Yronwood.png", "Stannis.gif", "Frey.png", "Hightower.gif", "Volantis.png", "Pentos.png", "Jofrey.png"
+    };
+    private static final String[] shieldsGreek = {"neutral.png", "Esparta.gif", "Atenas.gif", "Macedonia.gif", "Persia.gif", "Tracia.gif", "Milletus.gif", "Illyria.gif", "Epirus.gif"};
+    private static final String[] shieldsArzhog = {"neutral.png", "Twainek.gif", "Frusodian.gif"};
+
+    public ImageFactory() {
+        this.landmarkNames = localFacade.getTerrainLandmarksImage();
+    }
 
     public Image getTerrainImage(Local local) {
         int idxTerrain = ConverterFactory.terrainToIndex(localFacade.getTerrenoCodigo(local));
@@ -44,7 +82,7 @@ public class ImageFactory {
             //bordless meppa
             return new Image("/images/mapa/hex_2b_" + filename + ".gif");
         } else if (SettingsManager.getInstance().isConfig("MapTiles", "2d", "2b")) {
-            //bord meppa
+            //border meppa
             return new Image("/images/mapa/hex_" + filename + ".gif");
         } else if (SettingsManager.getInstance().isConfig("MapTiles", "3d", "2b")) {
             //3d from joao bordless
@@ -75,9 +113,6 @@ public class ImageFactory {
         return new Image("/images/mapa/hex_" + spanNames[direcao] + ".gif");
     }
 
-//    public Image getTracksImage(int direcao) {
-//        return new Image("/images/mapa/hex_" + tr[direcao] + ".gif");
-//    }
     public static Image getLandingImage(int direcao) {
         return new Image("/images/mapa/hex_" + landingNames[direcao] + ".gif");
     }
@@ -99,7 +134,105 @@ public class ImageFactory {
         return new Image("/images/mapa/cp_" + dockNames[dockSize] + ".gif");
     }
 
+    public Image getLandmark(String cdFeature) {
+        return new Image(this.landmarkNames.get(cdFeature));
+    }
+
     public static Image getCapitalImage() {
         return new Image("/images/mapa/cp_capital.gif");
+    }
+
+    public static Image getCharOtherImage() {
+        return new Image("/images/mapa/hex_pers_other.gif");
+    }
+
+    public static Image getCharAllyImage() {
+        return new Image("/images/mapa/hex_pers_ally.gif");
+    }
+
+    public static Image getCharImage() {
+        return new Image("/images/mapa/hex_personagem.gif");
+    }
+
+    public static Image getCharNpcImage() {
+        return new Image("/images/mapa/hex_npc.gif");
+    }
+
+    public static Image getItemLostImage() {
+        return new Image("/images/mapa/hex_artefato.gif");
+    }
+
+    public static Image getItemKnownImage() {
+        return new Image("/images/mapa/hex_artefatoKnown.gif");
+    }
+
+    public static Image getGoldmineImage() {
+        return new Image("/images/mapa/hex_goldmine.gif");
+    }
+
+    public static Image getShipsImage() {
+        return new Image("/images/mapa/hex_navio.gif");
+    }
+
+    public static Image getTagImage() {
+        return new Image("/images/mapa/hex_tag.gif");
+    }
+
+    public static Image getFogofwarImage() {
+        return new Image("/images/mapa/hex_fogofwar.gif");
+    }
+
+    public static Image getCombatBigNavyImage() {
+        return new Image("/images/combat_blue.png");
+    }
+
+    public static Image getCombatImage() {
+        return new Image("/images/combat.png");
+    }
+
+    public static Image getBlueBallImage() {
+        return new Image("/images/piemenu/dark_blue_button.png");
+    }
+
+    public static Image getYellowBallImage() {
+        return new Image("/images/piemenu/yellow_button.png");
+    }
+
+    public static Image getCombatBigArmyImage() {
+        return new Image("/images/combat_green.png");
+    }
+
+    public static Image getExplosionImage() {
+        return new Image("/images/explosion.png");
+    }
+
+    public static Image getAppIconImage() {
+        return new Image("/images/hex_wasteland.png");
+    }
+
+    public Image getArmyShield(Nacao nation, Cenario cenario) {
+        try {
+            return new Image("/images/armies/" + getExercitoStrings(WorldFacadeCounselor.getInstance().getCenario())[nationFacade.getNacaoNumero(nation)]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return new Image("/images/armies/" + getExercitoStrings(WorldFacadeCounselor.getInstance().getCenario())[0]);
+        }
+    }
+
+    private static String[] getExercitoStrings(Cenario scenario) {
+        if (scenario == null) {
+            return shieldsAll;
+        } else if (scenario.isGrecia()) {
+            return shieldsGreek;
+        } else if (scenario.isArzhog()) {
+            return shieldsArzhog;
+        } else if (scenario.isGot()) {
+            return shieldsGot;
+        } else if (scenario.isWdo()) {
+            return shieldsWdo;
+        } else if (scenario.isLom()) {
+            return shieldsLom;
+        } else {
+            return shieldsElse;
+        }
     }
 }
